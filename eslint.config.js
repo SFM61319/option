a0e790b26253a05673d27fb835ff5dd9c0b61e7f
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2024-07-08 18:09:27 +0530

    chore(config): Make ESLint respect `.gitignore`

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginJs from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  includeIgnoreFile(gitignorePath),
   eslintConfigPrettier,
 ];
